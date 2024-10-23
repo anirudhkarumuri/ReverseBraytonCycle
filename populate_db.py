8aa0a20be5c75e4b45d8cdc1d9b4c56c2507e893
sai.karumuri <anirudhkarumuri@gmail.com>
2024-10-23 21:07:43 +0200

    Adding air thermodynamic_properties.db to the repo

--- populate_db.py
+++ populate_db.py
@@ -6,18 +6,16 @@ from tabulate import tabulate
 # Using sqlite3 as no external sql servers are required, sqlite3 is embedded and comes with python package.
 
 # Step 1: Read the CSV file into a DataFrame
-csv_file_path = 'Air_properties.csv'  # Replace with your CSV file path
+csv_file_path = 'Air_properties.csv'
 df = pd.read_csv(csv_file_path, delimiter=';')
 
-print(tabulate(df, tablefmt="psql", headers="keys", showindex=False))
 # Step 2: Connect to SQLite database (it will create the database if it doesn't exist)
 db_file_path = 'thermodynamic_properties.db'
 conn = sqlite3.connect(db_file_path)
 
-table_name = 'properties'  # Specify the table name
+table_name = 'properties'
 df.to_sql(table_name, conn, if_exists='replace', index=False)
 query = "SELECT * FROM " + table_name
 df = pd.read_sql(query, conn)
-
-print(df)
+print(tabulate(df, tablefmt="psql", headers="keys", showindex=False))
 conn.close()
