7b347172dfaa1177a72a710156eaeb8cb6a9b522
sai.karumuri <anirudhkarumuri@gmail.com>
2024-10-23 20:56:45 +0200

    Store thermodynamic properties in db.

---
+++ populate_db.py
@@ -0,0 +1,23 @@
+import pandas as pd
+import tabulate
+import sqlite3
+from tabulate import tabulate
+
+# Using sqlite3 as no external sql servers are required, sqlite3 is embedded and comes with python package.
+
+# Step 1: Read the CSV file into a DataFrame
+csv_file_path = 'Air_properties.csv'  # Replace with your CSV file path
+df = pd.read_csv(csv_file_path, delimiter=';')
+
+print(tabulate(df, tablefmt="psql", headers="keys", showindex=False))
+# Step 2: Connect to SQLite database (it will create the database if it doesn't exist)
+db_file_path = 'thermodynamic_properties.db'
+conn = sqlite3.connect(db_file_path)
+
+table_name = 'properties'  # Specify the table name
+df.to_sql(table_name, conn, if_exists='replace', index=False)
+query = "SELECT * FROM " + table_name
+df = pd.read_sql(query, conn)
+
+print(df)
+conn.close()
