85194aa0292f1cf22547bb5a6f9fbb8af531dda6
sai.karumuri <anirudhkarumuri@gmail.com>
2024-10-24 08:12:14 +0200

    Added encoding to avoid errors.

--- populate_db.py
+++ populate_db.py
@@ -9,17 +9,15 @@ from tabulate import tabulate
 
 # Step 1: Read the CSV file into a DataFrame
 csv_file_path = 'Air_properties.csv'
-df = pd.read_csv(csv_file_path, delimiter=';')
+df = pd.read_csv(csv_file_path, delimiter=',', encoding='utf-8')
 
-if 'Data' not in os.listdir(os.getcwd()):
-    os.mkdir('Data')
-    # Step 2: Connect to SQLite database (it will create the database if it doesn't exist)
-    db_file_path = 'Data/thermodynamic_properties.db'
-    conn = sqlite3.connect(db_file_path)
+# Step 2: Connect to SQLite database (it will create the database if it doesn't exist)
+db_file_path = os.path.join(os.getcwd(),"Rev_Brayton_Cycle",'thermodynamic_properties.db')
+conn = sqlite3.connect(db_file_path)
 
-    table_name = 'properties'
-    df.to_sql(table_name, conn, if_exists='replace', index=False)
-    query = "SELECT * FROM " + table_name
-    df = pd.read_sql(query, conn)
-    print(tabulate(df, tablefmt="psql", headers="keys", showindex=False))
-    conn.close()
+table_name = 'properties'
+df.to_sql(table_name, conn, if_exists='replace', index=False)
+query = "SELECT * FROM " + table_name
+df = pd.read_sql(query, conn)
+print(tabulate(df, tablefmt="psql", headers="keys", showindex=False))
+conn.close()
